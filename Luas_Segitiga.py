6e810e08d72364f6331c32833330d5f61af8f04f
rekhanaya <144007097+rekhanaya@users.noreply.github.com>
2023-09-06 08:49:59 +0700

    Add files via upload

---
+++ Luas_Segitiga.py
@@ -0,0 +1,21 @@
+# Nama file: Luas_Segitiga.py
+# Pembuat: Rekha Inaya Putri
+# Tanggal: 3 September 2023
+# Deskripsi : Tentukan algoritma dan pseudocode luas segitiga (Alas = 25, Tinggi = 30)
+
+#def luas_segitiga (a,t):
+#return 1/2*a*t
+
+alas = 25
+tinggi = 30
+
+luas_segitiga = 1/2*alas*tinggi
+print(luas_segitiga)
+
+#Algoritma:
+#1. Buatlah sebuah fungsi dengan nama luas_segitiga dengan parameter a dan t
+#2. Masukkan rumus dari luas segiiga
+#3. Deklarasikan variabel alas dan tinggi
+#4. Selanjutnya tampilkan rumus
+#5. Panggil fungsi luas_segitiga
+#6.  Selesai
---
+++ Volume_Tabung.py
@@ -0,0 +1,21 @@
+# Nama file: Volume_Tabung.py
+# Pembuat: Rekha Inaya Putri
+# Tanggal: 3 September 2023
+# Deskripsi : Tentukan algoritma dan pseudocode volume tabung (jari-jari = 3, tinggi = 5)
+
+#def volume_tabung (r,t):
+#return 3.14*r*r*t
+
+jari = 3
+tinggi = 5
+
+volume_tabung = 3.14*jari*jari*tinggi
+print(volume_tabung)
+
+#Algoritma:
+#1. Buatlah sebuah fungsi dengan nama volume_tabung dengan parameter r dan t
+#2. Masukkan rumus dari volume tabung
+#3. Deklarasikan variabel jari-jari dan tinggi
+#4. Selanjutnya tampilkan rumus
+#5. Panggil fungsi Volume_Tabung
+#6.  Selesai
